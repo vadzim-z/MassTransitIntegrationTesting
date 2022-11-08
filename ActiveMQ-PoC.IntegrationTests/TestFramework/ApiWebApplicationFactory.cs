d2368e01dd9d1c00a2f6575ef15cad5b26e8646a
vadzim-z <vadzim.zylevich@maersk.com>
2022-11-08 14:01:53 +0100

    Added ActiveMQ-PoC.IntegrationTests project AB#143904

---
+++ ActiveMQ-PoC.IntegrationTests/TestFramework/ApiWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace ActiveMQ_PoC.IntegrationTests.TestFramework;
+
+public class ApiWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(config => { });
+        builder.ConfigureTestServices(services => { });
+    }
+}
